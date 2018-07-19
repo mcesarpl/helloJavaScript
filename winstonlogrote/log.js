702a1a96d2d1fe7554924d232afdbeeda3a64753
geisiane.franca <geisiane.franca@lsbd.ufc.br>
2018-07-19 15:32:24 -0300

    updated logger

--- winstonlogrote/log.js
+++ winstonlogrote/log.js
@@ -1,34 +1,49 @@
 'use strict';
 const
-    { createLogger, format, transports } = require('winston'),
-    { combine, timestamp, printf, colorize } = format,
+    { createLogger, format, transports, addColors } = require('winston'),
+    { combine, timestamp, printf, colorize, toUpperCase } = format,
     fs = require('fs'),
     logDir = 'log';
+
+
+const myCustomLevels = {
+  levels: { error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5 },
+  colors: { error: 'red', warn: 'yellow', info: 'white', verbose: 'green', debug: 'gray', silly:'gray'}
+};
+
 
 if (!fs.existsSync(logDir)) {
   fs.mkdirSync(logDir);
 }
 
+addColors(myCustomLevels.colors);
+
 const 
-    myFormat = printf(info => {
+    myConsoleFormat = printf(info => {
+        return `${info.timestamp} - [${info.level}] - ${info.message}`;
+    }),
+    myFileFormat = printf(info => {
         return `${info.timestamp} - [${info.level.toUpperCase()}] - ${info.message}`;
     }),
     logger = createLogger({
         transports: [
             new (transports.Console)({
-                format: combine(colorize(), myFormat),
+                levels: myCustomLevels.levels,
+                format: combine(colorize(), timestamp(), myConsoleFormat),
                 level: 'debug'
             }),
             new (transports.File)({
                 filename: `${logDir}/log.log`,
-                format: combine(timestamp(),myFormat),
+                format: combine(timestamp(),myFileFormat),
                 level: 'info'
             })
         ]
+    
     });
 
 module.exports = logger;
 
+logger.error('error logging');
 logger.info('info logging');
 logger.debug('debug logging');
 logger.warn('warn logging');
